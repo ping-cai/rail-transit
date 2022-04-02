f93ff572164aca6a50ed6beee90b31d707bb74df
ping-cai <928689419@qq.com>
2022-04-02 09:44:18 +0800

    只保留源码包

--- packaging/target/rail-transit-bin/sicau-rail_transit-1.0-SNAPSHOT-bin/bin/data-migration.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-spark-submit --class section.SectionMigration \
---master yarn --deploy-mode cluster \
---driver-memory 512M --executor-cores 2 \
---num-executors 1 \
---jars $(echo ../lib/*.jar | tr ' ' ',') \
-$(dirname $(pwd))/lib/data-migration-1.0-SNAPSHOT.jar
--- packaging/target/rail-transit-bin/sicau-rail_transit-1.0-SNAPSHOT-bin/bin/initDos.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-#初始化文本格式
-dos2unix $(dirname $(pwd))/bin/*.sh
